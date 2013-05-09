a7da93d5865be848fa23ed2f96f4903e5ad9b641
Laura <l.hodges@student.unsw.edu.au>
2013-05-09 12:04:40 +1000

    djashfudht

---
+++ src/CellGen.java
@@ -0,0 +1,58 @@
+/**
+ * This will probably inherit from the Cell class once we combine everything
+ * At the moment the methods are different but they can be re-named later if need be
+ * @author laura
+ *
+ */
+public class CellGen implements Cell{
+	public CellGen(int value){
+		this.value = value;
+		presence = true;
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		boolean result = false;
+		CellGen other;
+		
+		if(obj == this){
+			result =  true;
+		} else if (obj == null){
+			result = false;
+		} else if (obj.getClass() == CellGen.class){
+			other = (CellGen)obj;
+			if(other.value == this.value){
+				result = true;
+			}
+		} else if (obj.getClass() == int.class){
+			if(this.value == (int)obj){
+				result = true;
+			}
+		} 
+		return result;
+	}
+	
+	@Override
+	public int getValue(){
+		return value;
+	}
+	
+	@Override
+	public void setValue(int x){
+		value = x;
+	}
+	
+	@Override
+	public void setVisible(boolean x){
+		presence = x;
+	}
+	
+	@Override
+	public boolean isVisible(){
+		return presence;
+	}
+	
+	
+	private int value;
+	private boolean presence;
+}
