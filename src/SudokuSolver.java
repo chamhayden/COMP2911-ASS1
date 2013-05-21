b597a0601e400c7d1277fc711813cf031f697d3a
Steven Falconieri <zmotu@yahoo.com.au>
2013-05-21 11:03:53 +1000

    Added Sudoku Solver which implements Unique Checker and uses Sudoku State

---
+++ src/SudokuSolver.java
@@ -0,0 +1,10 @@
+
+public class SudokuSolver implements UniqueChecker {
+
+	@Override
+	public boolean isSudokuSolutionUnique(int[][] board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
