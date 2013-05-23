f68cf44f8bc0e3aea89317be69a97f361a237ac7
Hayden Smith <mail@chambalam.com>
2013-05-23 22:01:48 +1000

    Board Interface FInished

--- src/Board.java
+++ src/Board.java
@@ -7,30 +7,92 @@
 */
 public interface Board
 {
-	
-	// Cell Values
+	/**
+	 * Get the value that is currently held within a particular cell
+	 * @param row Row cell is in
+	 * @param col Column cell is in
+	 * @return Value currently held in the cell
+	 */
 	public int getCellValue(int row, int col);
+	
+	/**
+	 * Set the value that is currently held within a particular cell
+	 * @param row Row cell is in 
+	 * @param col Column cell is in
+	 * @param number Value currently held in the cell
+	 */
 	public void setCellValue(int row, int col, int number);
 	
-	// Cell Visiblity
+	/**
+	 * Determine whether a cell is currently visible for the current 
+	 *  state of the board
+	 * @param row Row cell is in
+	 * @param col Column cell is in
+	 * @return Whether the cell is currently visible at the current
+	 *  particular state of the board
+	 */
 	public boolean isCurrentlyVisibleCell(int row, int col);
+	
+	/**
+	 * Determine whether a cell is visible at the initial state 
+	 *  of the board
+	 * @param row Row cell is in
+	 * @param col Column cell is in
+	 * @return Whether the cell is visible at the initial state
+	 *  of the board
+	 */
 	public boolean isInitiallyVisibleCell(int row, int col);
+	
+	/**
+	 * Set the visiblity of a cell on the board
+	 * @param row Row cell is in
+	 * @param col Column cell is in
+	 * @param visiblity Whether the particular cell is visible or not
+	 */
 	public void setCellVisiblity(int row, int col, boolean visiblity);
 	
 	// Cell Temp Values
 	public boolean isVisibleCellTemp(int row, int col, int number);
 	public void setCellTemp(int row, int col, int number, boolean isSet);
 	
-	// Correctness
+	/**
+	 * Return whether the board has been correctly filled out
+	 * @return Whether the board has been correctly filled out
+	 */
 	public boolean isCorrectBoard();
+	
+	/**
+	 * Return whether a particular cell in the board has been
+	 *  correctly filled out 
+	 * @param row Row cell is in
+	 * @param col Column cell is in
+	 * @return Whether a particular cell in the board has been correctly
+	 *  filled out
+	 */
 	public boolean isCorrectCell(int row, int col);
 	
-	// Difficulty
+	/**
+	 * Return the difficulty constant for "Easy"
+	 * @return Difficulty constant for "Easy"
+	 */
 	public int difficultyValueEasy();
+	
+	/**
+	 * Return the difficulty constant for "Medium"
+	 * @return Difficulty constant for "Medium"
+	 */
 	public int difficultyValueMedium();
+	
+	/**
+	 * Return the difficulty constant for "Hard"
+	 * @return Difficulty constant for "Hard"
+	 */
 	public int difficultyValueHard();
 	
-	// Board Size
+	/**
+	 * Get the size of the board
+	 * @return Size of the board
+	 */
 	public int getBoardSize();
 	
 }
