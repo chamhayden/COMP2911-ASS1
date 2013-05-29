c7e9e05fffe5e628b5e233a0ed4d69745a334810
Jerome <jerome.br@gmail.com>
2013-05-29 17:12:57 +1000

    latest

--- src/SudokuManager.java
+++ src/SudokuManager.java
@@ -2,6 +2,8 @@ import java.awt.EventQueue;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 
 
@@ -9,6 +11,22 @@ public class SudokuManager {
 	
 	public SudokuManager()
 	{
+		
+		/*try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }*/
+        // Turn off metal's use of bold fonts 
+        //UIManager.put("swing.boldMetal", Boolean.FALSE);
 		
 		board = new SudokuBoard(BOARD_SIZE);
 		
@@ -32,10 +50,12 @@ public class SudokuManager {
 				frame.addWindowListener(new WindowAdapter() {
 					// handle window closing 
 					public void windowClosing(WindowEvent we) {
-				        if(pane.exitMessageInGame())
-				        	System.exit(0);
-				    }
-					});
+						if(pane.exitMessageInGame())
+							System.exit(0);
+					}
+				});
+				//frame.addKeyListener(new Listeners(frame).inputShortCut());
+				frame.pack();
 				frame.setVisible(true);
 			}
 		});
