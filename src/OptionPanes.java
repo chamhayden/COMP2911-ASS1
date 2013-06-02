2795728386d25ba8478d52298d4c006a5939f52f
Jerome <jerome.br@gmail.com>
2013-06-02 16:02:10 +1000

    uml

--- src/OptionPanes.java
+++ src/OptionPanes.java
@@ -173,10 +173,26 @@ public class OptionPanes {
 	
 	/** Returns an ImageIcon, or null if the path was invalid. 
 	 *  
-	 *  method below copied from the link below - all redistribution conditions met:
+	 *  method below copied from the Java tutorial link below - all redistribution conditions met:
 	 *  http://docs.oracle.com/javase/tutorial/displayCode.html?code=http://docs.oracle.com/javase/tutorial/uiswing/examples/components/LabelDemoProject/src/components/LabelDemo.java
-	 *  Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
 	 * 
+	 * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
+	 *
+	 * Redistribution and use in source and binary forms, with or without
+	 * modification, are permitted provided that the following conditions
+	 * are met:
+	 *
+	 *   - Redistributions of source code must retain the above copyright
+	 *     notice, this list of conditions and the following disclaimer.
+	 *
+	 *   - Redistributions in binary form must reproduce the above copyright
+	 *     notice, this list of conditions and the following disclaimer in the
+	 *     documentation and/or other materials provided with the distribution.
+	 *
+	 *   - Neither the name of Oracle or the names of its
+	 *     contributors may be used to endorse or promote products derived
+	 *     from this software without specific prior written permission.
+	 *
 	 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
 	 * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 	 * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -189,6 +205,7 @@ public class OptionPanes {
 	 * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 	 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 	 */
+	
     protected static Icon createImageIcon(String path, String description) {
         java.net.URL imgURL = OptionPanes.class.getResource(path);
         if (imgURL != null) {
